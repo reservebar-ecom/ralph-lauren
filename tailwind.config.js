49b9ab8700f2b1b9316500be3cdf85476338ed88
Lucas Borges <lucaspdfborges@gmail.com>
2024-04-22 21:50:22 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        leJeune: ['"LeJeuneDeck"'],
+        foundersGrotesk: ['"FoundersGrotesk"'],
+      },
+    },
+  },
+  plugins: [],
+};
